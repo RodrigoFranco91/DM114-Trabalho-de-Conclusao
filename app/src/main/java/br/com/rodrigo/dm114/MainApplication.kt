8a9f86f3b9d0d2a35fca21186caa38b936872157
Rodrigo Franco <rodrigofrancodelima@gmail.com>
2020-07-25 17:46:54 -0300

    Criando servicos para fazer Request. 105p

---
+++ app/src/main/java/br/com/rodrigo/dm114/MainApplication.kt
@@ -0,0 +1,19 @@
+package br.com.rodrigo.dm114
+
+import android.app.Application
+import android.content.Context
+
+class MainApplication: Application() {
+
+    init {
+        instance = this
+    }
+
+    companion object {
+        private var instance: MainApplication? = null
+
+        fun getApplicationContext(): Context {
+            return instance!!.applicationContext
+        }
+    }
+}
