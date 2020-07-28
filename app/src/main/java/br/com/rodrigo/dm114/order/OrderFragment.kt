d7e94ade73e522d6565d1be3943da0bcbfc158d7
Rodrigo Franco <rodrigofrancodelima@gmail.com>
2020-07-28 10:51:23 -0300

    Order Fragment feito

--- app/src/main/java/br/com/rodrigo/dm114/order/OrderFragment.kt
+++ app/src/main/java/br/com/rodrigo/dm114/order/OrderFragment.kt
@@ -9,17 +9,20 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.DividerItemDecoration
-import br.com.rodrigo.dm114.databinding.OrderBinding
+import br.com.rodrigo.dm114.databinding.FragmentOrderBinding
 import androidx.recyclerview.widget.RecyclerView.VERTICAL
+import br.com.rodrigo.dm114.R
 
 private const val TAG = "ProductsListFragment"
 class OrderFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val binding = OrderBinding.inflate(inflater)
+        val binding = FragmentOrderBinding.inflate(inflater)
         binding.setLifecycleOwner(this)
 
         val itemDecor = DividerItemDecoration(getContext(), VERTICAL);
         return binding.root
     }
+
+    //inflater.inflate(R.layout.fragment_order, container, false )
 }
