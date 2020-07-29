1d1dd7ec4334d04d77fb727b7103f756383774b3
Rodrigo Franco <rodrigofrancodelima@gmail.com>
2020-07-29 01:59:38 -0300

    Recebendo notificacoes com sucesso!

--- app/src/main/java/br/com/rodrigo/dm114/order/OrderViewModel.kt
+++ app/src/main/java/br/com/rodrigo/dm114/order/OrderViewModel.kt
@@ -1,6 +1,10 @@
 package br.com.rodrigo.dm114.order
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import br.com.rodrigo.dm114.persistence.Order
 
 class OrderViewModel : ViewModel() {
+    val fcmRegistrationId = MutableLiveData<String>()
+    val product = MutableLiveData<Order>()
 }
